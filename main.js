7943f3d8d68bc525e5117e0459f64110b4d63516
Magdy Elsayyad <medoelsayad229@gmail.com>
2023-06-03 16:56:35 +0300

    uu

--- main.js
+++ main.js
@@ -5,17 +5,23 @@ window.addEventListener("DOMContentLoaded", function () {
   const box = this.document.getElementById("order_btn").querySelector(".box");
   const road = this.document.getElementById("order_btn").querySelector(".road");
 
+  const backSound = new Audio("./assets/sounds/back.mp3");
+  const exitSound = new Audio("./assets/sounds/exit.mp3");
   btn.addEventListener("click", () => {
     text.classList.add("hide");
 
     road.classList.add("show");
     car.classList.add("show");
     box.classList.add("show");
+    backSound.play();
     car.classList.add("back");
     box.classList.add("put");
     setTimeout(() => {
+      backSound.pause();
+      exitSound.play();
+
       car.classList.add("walk");
-    }, 1000);
+    }, 2000);
     setTimeout(() => {
       car.classList.add("exit");
       box.classList.add("exit");
@@ -24,8 +30,8 @@ window.addEventListener("DOMContentLoaded", function () {
         text.classList.remove("hide");
         text.textContent = "Order on the way to you 😊";
         text.classList.add("show");
-        btn.setAttribute("disabled", true)
+        btn.setAttribute("disabled", true);
       }, 500);
-    }, 2000);
+    }, 3000);
   });
 });
